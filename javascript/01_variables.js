453de49827520cd971f0b2153af4a39f224f9cb4
naman1729 <namangautam000@gmail.com>
2023-09-11 22:53:23 +0530

    Adding Javascript_learning

---
+++ javascript/01_variables.js
@@ -0,0 +1,17 @@
+// const is used for variables that will not change ... ever ... like pi or the speed of light
+const accountId = 1234
+
+// let is used for variables that will change   ... like the current temperature
+let accountEmail = "xyz@gmail.com"
+
+// var is used for variables that will change ... like the current temperature
+var accountPassword = "meNhiBataunga1729"
+
+accountCity = "Agra"
+
+// accountId = 5678 // this will throw an error
+accountEmail = "yzx@gmail.com" 
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
