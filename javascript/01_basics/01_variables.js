ef4388bcbbe4f0d9ac7ce4e4383206f8f8827a8c
naman1729 <namangautam000@gmail.com>
2023-09-12 00:16:17 +0530

    update

---
+++ javascript/01_basics/01_variables.js
@@ -0,0 +1,23 @@
+// const is used for variables that will not change ... ever ... like pi or the speed of light
+const accountId = 1234;
+
+// let is used for variables that will change   ... like the current temperature
+let accountEmail = "xyz@gmail.com";
+
+// var is used for variables that will change ... like the current temperature
+/* - var is the old way of declaring variables in JS because it has some issues like hoisting in which the variable is declared at the top of the code even if it is declared at the bottom of the code ...
+    this can cause some issues in the code like modifying the value of a variable before it is declared ... this is why we use let and const instead of var 
+   - In simple words, issue in block scope and function scope
+   */
+var accountPassword = "meNhiBataunga1729";
+accountCity = "Agra";
+let accountState;
+
+// accountId = 5678 // this will throw an error because we cannot change a const variable
+accountEmail = "yzx@gmail.com";
+accountPassword = "meNhiBataunga229169";
+accountCity = "Delhi";
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ javascript/01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version of JS
+
+// alert(3 + 4); // we are use nodejs, not browser, so we can't use alert() function for showing output like popup .
+
+let name = "Virat Kohli"; // variables can store strings
+let age = 32; // variables can store numbers of range -(2^53 - 1) to (2^53 - 1) 
+let isCaptain = true; // boolean value (true or false)
+let state = null; // null value
+let address; // undefined value
+
+// bigInt
+//  string => "" (name mentioned above)
+// null => standalone value (state mentioned above)
+// undefined => value is not assign yet (address mentioned above)
+// symbol => unique 
+
+console.log(typeof null); // output => object
+
+console.log(typeof undefined); // output => undefined
