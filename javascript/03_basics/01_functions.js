96b3bf575f88198c1ab1183348d20df9ef6cf2ff
naman1729 <namangautam000@gmail.com>
2023-09-23 00:23:08 +0530

    update

--- javascript/03_basics/01_functions.js
+++ javascript/03_basics/01_functions.js
@@ -34,4 +34,42 @@ function loginUserMessage(name = "Naman") {
   return `${name} logged in successfully`;
 }
 
-console.log(loginUserMessage());
+//  console.log(loginUserMessage());
+
+// rest operator is used to take multiple arguments
+// function calculateCartPrice(...num1) {
+//   return num1;
+// }
+
+// console.log(calculateCartPrice(2, 3, 4, 5));
+
+function calculateCartPrice(val1, val2, ...num1) {
+  return num1;
+}
+
+// console.log(calculateCartPrice(2, 3, 4, 5));
+
+const user = {
+  userName: "Virat Kohli",
+  price: 100,
+};
+
+function handleObject(anyObject) {
+  console.log(
+    `user name is ${anyObject.userName} and price is ${anyObject.price}`
+  );
+}
+
+// handleObject(user);
+handleObject({
+  userName: "Sunil Chhetri",
+  price: 100,
+});
+
+const myNewArray = [100, 200, 300, 400];
+
+function handleArray(anyArray) {
+  return anyArray[1];
+}
+
+console.log(handleArray(myNewArray));
